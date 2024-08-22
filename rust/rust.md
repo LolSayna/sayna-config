0facf1a9288d5a8f8a2f8ab5329f18006d5977e3
Mareike Burg <mareike.burg@web.de>
2024-08-22 21:46:10 +0200

    learning rust chap 3 done

--- rust/rust.md
+++ rust/rust.md
@@ -9,5 +9,34 @@ New Project: `cargo new NAME` \
 Check/Build/Execute: `cargo check/build/run` \
 Release:: `cargo build --release`
 
-## Datatypes
+Cargo.toml = Dependencies in a broad sense 
 
+Cargo.lock = Exact information about your dependencies
+> put into Git so everyone builds with the same version, without hardcoding the versions
+
+## Keywords
+use | fn/return | let (mut) | const [global scope possible] | shadowing [reuse var name, also change type]
+
+println!()
+
+match,| expect Ok/Err | cmp + Ordering::
+
+loop [loop labels to jump between inner and outer loops] | break | continue | if/else/else if | while | for
+
+String::new/from | 
+
+## Datatype
+i8 / u8 -> i128 / u128 oder isize/usize for architecure dependend \
+overflows panic at debug mode, at release not!
+
+f32 / f64 | bool | char [unicode]
+
+tupel [any types, can be unpacked or .0 .1] | arrays [same typ, fixed length]
+
+## Conzepts
+
+### Statement vs Expression
+- **Statements** are instructions that perform some action and do not return a value.
+- **Expressions** evaluate to a resultant value.
+
+In function implicite return the last expression, no ; needed.
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,41 +1,72 @@
 use ferris_says::say;
 use rand::Rng;
 use std::cmp::Ordering;
+
+// get library into scope
 use std::io;
+// use specific Module
 use std::io::{stdout, BufWriter};
 
-/* Zahlenratenspiel */
+/* Main function, always gets called first.
+ * Declaration order irelevant.
+ * */
 fn main() {
-    println!("Rate die Zahl!");
+    // game();
 
-    // range inklusive (1..493) wäre exklusiv
+    let mut userin = String::new();
+    io::stdin()
+        .read_line(&mut userin)
+        .expect("Fehler bei Eingabe");
+    let userin: f64 = match userin.trim().parse() {
+        Ok(num) => num,
+        Err(_) => -1.0, // _ means wildcard
+    };
+    let fah = convert_to_fahrenheit(userin);
+
+    println!("C: {userin} and Fahrenheit: {fah}");
+}
+
+fn convert_to_fahrenheit(c: f64) -> f64 {
+    c * 1.8 + 32.0
+}
+
+fn game() {
+    // range inclusive!
+    // (1..493) would be exclusive 493
     let sec = rand::thread_rng().gen_range(1..=493);
 
-    // TODO remove later
+    // print macro incl placeholder variable
     println!("GEHEIMNIS: {sec}");
 
     loop {
         println!("Gebe deine Zahl ein:");
 
+        // mut keyword, without vars are immutable
         let mut gue = String::new();
+        // read_line returns "Result", expect triggers if "Result" = Err
         io::stdin().read_line(&mut gue).expect("Fehler bei Eingabe");
 
-        // what a fancy match syntax :O
+        // shadow gue -> reuse var name + parse into u32
+        // if it fails continue the main loop
         let gue: u32 = match gue.trim().parse() {
             Ok(num) => num,
             Err(_) => continue, // _ means wildcard
         };
 
-        println!("Dein Tipp ist: {gue}");
+        // match comparison
         match gue.cmp(&sec) {
             Ordering::Less => println!("Zu klein!"),
             Ordering::Greater => println!("Zu groß!"),
             Ordering::Equal => {
+                win(gue);
                 break;
             }
         }
     }
+}
 
+// fn with parameter, _ indicated value is not used
+fn win(_num: u32) {
     let stdout = stdout();
     let message = String::from("Du hast gewonnen");
     let w = message.chars().count();
