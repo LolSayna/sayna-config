c04834ccec5b4b4f93624e0d09ca0cbf7f9299cf
Mareike Burg <mareike.burg@web.de>
2024-12-28 13:00:10 +0100

    Changed esa tmux script

--- esa.sh
+++ esa.sh
@@ -2,12 +2,14 @@
 # Create a tmux session for my esa workflow
 
 SESSION_NAME="esa"
-WINDOW_NAMES=("Ciarc" "Images" "ssh-Console")
+WINDOW_NAMES=("Ciarc" "Helper" "ssh-Console")
 # Prepend space before cmd to not put them into history
 STARTUP_CMDS=(" cd ~/Desktop/esa/CIARC/" \
             " cd ~/Desktop/esa/CIARC/logs/melvonaut/images/" \
             " ssh console")
 
+SECOND_WINDOW_CMDS=(" cd ~/Desktop/esa/CIARC/" \
+                    " poetry run rift-console run-server")
 
 # Check if the session already exists
 tmux has-session -t $SESSION_NAME 2>/dev/null
@@ -17,11 +19,17 @@ if [ $? != 0 ]; then
     tmux new-session -d -s $SESSION_NAME -n "${WINDOW_NAMES[0]}"
     tmux send-keys -t $SESSION_NAME:1 "${STARTUP_CMDS[0]}" C-m
   
-    # Create other windows  
-    for ((i=1; i<${#WINDOW_NAMES[@]}; i++)); do
-        tmux new-window -t $SESSION_NAME -n "${WINDOW_NAMES[i]}"
-        tmux send-keys -t $SESSION_NAME:$((i+1)) "${STARTUP_CMDS[i]}" C-m
-    done
+    # Second window
+    tmux new-window -t $SESSION_NAME -n "${WINDOW_NAMES[1]}"
+    tmux send-keys -t $SESSION_NAME:2 "${STARTUP_CMDS[1]}" C-m
+
+    tmux split-window -v -t $SESSION_NAME:2
+    tmux send-keys -t $SESSION_NAME:2.2 "${SECOND_WINDOW_CMDS[0]}" C-m
+    tmux send-keys -t $SESSION_NAME:2.2 "${SECOND_WINDOW_CMDS[1]}" C-m
+
+    # Third window
+    tmux new-window -t $SESSION_NAME -n "${WINDOW_NAMES[2]}"
+    tmux send-keys -t $SESSION_NAME:3 "${STARTUP_CMDS[2]}" C-m
 
 fi
 
@@ -33,4 +41,4 @@ tmux attach-session -t $SESSION_NAME
 
 # There is still a bug in my tmux.conf with window names, reloading it fixed it
 # wait for 1s then reloading config. & is for nonblocking of this line
-(sleep 1 && tmux source-file ~/.config/tmux/tmux.conf) &
+(sleep 10 && tmux source-file ~/.config/tmux/tmux.conf) &
