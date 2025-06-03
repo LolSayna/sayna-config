405df2c66465f587904daad11e341d00772ae29b
Mareike Burg <mareike.burg@web.de>
2025-06-03 11:36:37 +0200

    Working on dotfiles.sh

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Also my wiki and cooking recipe book.
 
 ## Todos
 - benchmarks testen [video](https://youtu.be/y9tS_qHX5sE?si=DJxWwku_dGwR4ek6)
-- neovim/lazygit testen? neovim kaputt!, lazy-lock.json - gitingore?
+- neovim testen vm
+- tmux kapputt
---
+++ setup/dotfiles.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+# Install script for my dotfiles
+
+CONF="$HOME/sayna-config/dotfiles"
+
+# [PATHS]
+# NAME - SOURCE - DESTINATION
+paths=( 
+    "Alacritty $CONF/alacritty.toml $XDG_CONFIG_HOME/alacritty/alacritty.toml"
+    "Zshenv $CONF/.zshenv $HOME/.zshenv"
+    "Zshrc $CONF/zsh/.zshrc $XDG_CONFIG_HOME/zsh/.zshrc"
+    "Zshrc-local $CONF/zsh/.zshrc.local $XDG_CONFIG_HOME/zsh/.zshrc.local"
+    "P10k $CONF/zsh/.p10k.zsh $XDG_CONFIG_HOME/zsh/.p10k.zsh"
+    "Tmux $CONF/tmux.conf $XDG_CONFIG_HOME/tmux/tmux.conf"
+    "Code-Settings $CONF/code/settings.json $XDG_CONFIG_HOME/Code/User/settings.json"
+    "Code-Keybindings $CONF/code/keybindings.json $XDG_CONFIG_HOME/Code/User/keybindings.json"
+    "Neovim $CONF/nvim/ $XDG_CONFIG_HOME/nvim/"
+    "Gdbinit $CONF/gdbinit $XDG_CONFIG_HOME/gdb/gdbinit"
+    )
+echo "Found ${#paths[@]} config files."
+
+# [FUNCTIONS]
+usage() {
+    echo "Usage: $0 [-write] [-read] [-overwrite]"
+}
+write_config() {
+    for file in "${paths[@]}"
+    do
+        IFS=' ' read -r name source destination <<< "$file"
+        if test -e $destination; then
+            echo "$name exists, not updated."
+        else 
+            echo "Wrote $name to $destination."
+            cp -r $source $destination
+        fi
+    done
+}
+read_config() {
+    echo "Usage: $0 [-write] [-read] [-overwrite]"
+}
+overwrite() {
+    echo "Usage: $0 [-write] [-read] [-overwrite]"
+}
+
+# [PARSER]
+if [ $# -eq 0 ];then
+    usage
+    exit 0
+fi
+while [[ $# -gt 0 ]]; do
+    arg="$1"
+    case $arg in
+        -write)
+            write_config
+            shift
+            ;;
+		-read)
+            read_config
+            shift
+            ;;
+		-overwrite)
+            overwrite
+            shift
+            ;;
+        -h|--help)
+            usage
+            exit 0
+            ;;
+        *)
+            echo "Unknown option: $arg"
+            usage
+            exit 1
+            ;;
+    esac
+done
