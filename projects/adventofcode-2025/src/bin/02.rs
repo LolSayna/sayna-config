42fa81ebf7180fef414a6a5d0968a5ebc5bcfa28
Sayna <mareike.burg@web.de>
2025-12-02 14:42:25 +0100

    aoc25-2

--- projects/adventofcode-2025/src/bin/02.rs
+++ projects/adventofcode-2025/src/bin/02.rs
@@ -16,42 +16,68 @@ fn first(input: &str) -> usize {
         // println!("{start} {end}");
 
         for id in start..=end {
-            println!("\n{id}");
-            for x in 1..=id.to_string().len()/2 {
-                let s: String = id.to_string();
-                println!("s: {s} x: {x}");
-                
-                let (pat, mut remain) = s.split_at(x);
-                println!("Pat: {pat} Remain: {remain}");
+            // println!("\n{id}");
 
-                if pat.len() != remain.len() {
-                    println!("Wrong len!");
-                    // (_, remain) = remain.split(x);
-                    println!("Pat: {pat} Remain: {remain}");
-                }
-
-                if pat == remain {
-                    println!("Invalid!");
-                    invalid += id;
-                }
-                // let remain: String = s.split_off(x);
-                // println!("Remain: {remain}");
-
-                // let (y, _) = remain.split_at(x);
-                // if pat == y {
-                //     println!("Invalid!");
-                //     invalid += id;
-                // }
-            }
-            
+            let s: String = id.to_string();
+            let (first, half) = s.split_at(s.len()/2);
+            if first == half {
+                // println!("Invalid!");
+                invalid += id;
+            }    
         }
     }
     invalid
 }
 
-// fn second(input: &str) -> usize {
+fn second(input: &str) -> usize {
 
-// }
+    let v: Vec<&str> = input.split_terminator(',').collect();
+    let mut invalid: usize = 0;
+
+    for range in v {
+        // println!("{range}");
+        let mut it = range.split_terminator('-');
+        let start = it.next().unwrap().parse::<usize>().unwrap();
+        let end = it.next().unwrap().parse::<usize>().unwrap();
+        // println!("{start} {end}");
+
+        for id in start..=end {
+            
+            // println!("\n{id}");
+            
+            for x in 1..=id.to_string().len()/2 {
+
+                let s: String = id.to_string();
+                if s.len() % x != 0 {
+                    continue;
+                }
+
+                let mut is_valid: bool = false;
+                let len = s.len();
+                let mut cur: usize = 0;
+                // println!("s: {s} x: {x} len: {len}");
+                
+                while cur+x+x <= len {
+                    // println!("cur: {cur} first: {} sec: {}", &s[cur..cur+x], &s[cur+x..cur+x+x]);
+
+                    if &s[cur..cur+x] != &s[cur+x..cur+x+x] {
+                        // println!("Valid!");
+                        is_valid = true;
+                        break;
+                    }
+                    cur += x;
+                }
+
+                if !is_valid {
+                    // println!("Invalid: {id}");
+                    invalid += id;
+                    break;
+                }
+            }         
+        }
+    }
+    invalid
+}
 
 fn main() {
     println!("Starting Challenge: {DAY}");
@@ -61,5 +87,5 @@ fn main() {
     // println!("Input:  {input}");
 
     println!("Part 1 solution is: {}", first(&input));
-    // println!("Part 2 solution is: {}", second(&input));
+    println!("Part 2 solution is: {}", second(&input));
 }
